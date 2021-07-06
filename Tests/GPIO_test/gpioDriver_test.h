a49a53ab2a88d2af982088225ac2d88d2b2b8966
Robert Klajko <robert_klajko@epam.com>
2021-07-06 18:05:06 +0200

    GPIO driver test pins amended

--- Tests/GPIO_test/gpioDriver_test.h
+++ Tests/GPIO_test/gpioDriver_test.h
@@ -8,9 +8,10 @@
 
 #include "gpioDriver.h"
 
-#define LED1_GPIO 26
-#define LED2_GPIO 25
+#define LED1_GPIO 25
+#define LED2_GPIO 23
 #define BUTTON1_GPIO 34
+#define BUTTON2_GPIO 35
 #define PIN_TOGGLE_DELAY_MS 1000
 
 void gpio_driver_out_task(void *pvParameter);
